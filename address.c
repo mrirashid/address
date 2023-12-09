c2cd9e50755af5931adb3b540a181f565122ac15
mri rashid <mdislam0996@gmail.com>
2023-12-09 13:04:39 +0800

    first commit

---
+++ address.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+printf("Full Name: Md Rashidul Islam\n");
+printf("Street Address:Poduar Bazar, Bishowroad\n");
+printf("City: Cumilla\n");
+printf("State: Chottogram\n");
+printf("Zip Code:3500");
+return 0;
+}
